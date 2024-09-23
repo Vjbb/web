f60cd8c1fafd86a3605bbde7c93ab2a48aea2806
SMukagali <161022573+SMukagali@users.noreply.github.com>
2024-09-23 13:29:35 +0500

    header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,38 @@ function App() {
   return (
       <div className='App'>
         <header className='App-header'>
-          
+        <div class="header-container">
+            <div class="header-container2">
+                <div class="logoimg">
+                    <div>
+                        <a href="">
+                            <img src="./picture/Preview.png" alt=""/>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="addition">
+                    <div class="navigation">
+                        <div><a href="">Home</a></div>
+                        <div><a href="">About</a></div>
+                        <div><a href="">Sales</a></div>
+                        <div><a href="">Contact</a></div>
+                    </div>
+
+
+                    <div class="search1">
+                        <div class="header-search">
+                                <input type="text"/>
+                            </div>
+                            <div class="header-basket">
+                                <a href="#" class="cart-icon">
+                                    🛒
+                                </a>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
         </header>
       </div>
   );
